006d40dc5c252b42fe281fec5eabffba87e53d6b
janbar <janbar@nmu.edu>
2024-10-03 18:59:31 -0400

    update map test to count tiles to verfy map is being fully initialized

--- mimic/src/modules/map_test.rs
+++ mimic/src/modules/map_test.rs
@@ -21,11 +21,17 @@ fn test_constructor() {
     };
     let a_map = map::Map::new(&a_map_attrs);
 
+    let mut tile_count = 0;
     assert_eq!(a_map.map.len(), a_map_attrs.length);
     for row in a_map.map{
         assert_eq!(row.len(), a_map_attrs.width);
         for col in row {
+            tile_count += 1;
             assert_eq!(col.height, a_map_attrs.base_tile_height);
         }
     }
+
+    let map_length: i32 = a_map_attrs.length.clone() as i32;
+    let map_width: i32 = a_map_attrs.width.clone() as i32;
+    assert_eq!(tile_count, map_length * map_width)
 }
