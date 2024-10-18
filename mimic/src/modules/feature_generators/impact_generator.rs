7bc110c1608731617d1ed7a7c873ca8f12bd1a8e
Sage <lmaoofers1@gmail.com>
2024-10-18 14:15:59 -0400

    put some comments in impact generator, revised impact generator algorithm

--- mimic/src/modules/feature_generators/impact_generator.rs
+++ mimic/src/modules/feature_generators/impact_generator.rs
@@ -16,31 +16,45 @@ impl Default for ImpactGenerator {
 }
 
 impl ImpactGenerator {
+    //maybe move amount to throw rng to map generator -> would remove an extra arg pass to ImpactGenerator
     fn generate(a_map: &mut Map, frequency: &Distribution, depth_range: &Distribution) {
+        //initialize ImpactGenerator
+
+        //loop for number of impacts to generate
+            //generate crater
+        
+        //place_undistributed_material()
+    }
+
+    pub fn generate_crater(a_map: &mut Map, crater_depth: u16, impact_coord: Coordinate) {
+        //calculate radius
+        //...
+    }
+
+    //set tile in the crater's height to height of the tile at the impact coordinate
+    pub fn level_tile(a_map: &mut Map, tile_coord: Coordinate) {
 
     }
 
-    pub fn generate_crater(a_map: &mut Map, crater_depth: u16, a_coord: Coordinate) {
+    //set tile in the crater's height to the right depth based off distance from impact coordinate and 
+    //crater depth
+    pub fn set_tile_depth(a_map: &mut Map, dist_from_center: u16, crater_depth: u16, tile_coord: Coordinate) {
 
     }
 
-    pub fn level_crater_site(a_map: &mut Map, crater_radius: u16, a_coord: Coordinate) {
-
-    }
-
-    pub fn dig_crater(a_map: &mut Map, crater_radius: u16, crater_depth: u16, a_coord: Coordinate) {
-
-    }
-
+    //maybe unnecessary
     pub fn build_crater_lip(a_map: &mut Map, crater_radius: u16, crater_depth: u16, a_coord: Coordinate) {
 
     }
 
-    pub fn place_undsitributed_height(a_map: &mut Map) {
+    //for redistributing eroded material back around the map
+    //for Mimic V1, material just means units of height 
+    pub fn place_undsitributed_material(a_map: &mut Map) {
 
     }
 
-    pub fn calculate_undistributed_height(a_map: &mut Map) {
+    //maybe unnecessary
+    pub fn calculate_undistributed_material(a_map: &mut Map) {
 
     }
 }
